102617ccdd76f5e2eb35a244ec648ad278c5e0dd
Amrut <amrutpanda7789@gmail.com>
2020-06-29 19:50:26 +0530

    ADDED: Given india state census csv file return exact records

---
+++ src/test/java/com/bridgelabz/censusanalysertest/CensusAnalyserTest.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.censusanalysertest;
+
+import com.bridgelabz.censusanalyser.exception.CensusAnalyserException;
+import com.bridgelabz.censusanalyser.service.CensusAnalyser;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CensusAnalyserTest {
+
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+
+    CensusAnalyser censusAnalyser;
+
+    @Before
+    public void create_ObjectOf_CensusAnalyser() {
+        censusAnalyser = new CensusAnalyser();
+    }
+
+    @Test
+    public void givenIndianStateCensusCSVFile_ShouldReturnsCorrectRecords() {
+        try {
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(29,numOfRecords);
+        } catch (CensusAnalyserException e) { }
+    }
+}
